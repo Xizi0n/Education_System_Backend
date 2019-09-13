a6598ff19aecc71c2ceedf81ad3a6e56c870bda2
Xizi0n <nagyadam9415@gmail.com>
2019-09-13 10:42:34 +0200

    Adding https

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+var fs = require('fs');
+var https = require('https');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
@@ -31,8 +33,12 @@ mongoose
     "mongodb+srv://adam:GKLCVf35uvgx3Bev@cluster0-p72yj.mongodb.net/tudastar"
   )
   .then(() => {
-    app.listen(4001, () => {
-      console.log("App runnning");
+    https.createServer({
+      key: fs.readFileSync('server.key'),
+      cert: fs.readFileSync('server.cert')
+      }, app)
+        .listen(4001, () => {
+          console.log("App runnning");
     });
   })
   .catch(err => {
